d4490ec42e338748daf36419df30c121984da30c
Soros Chok <25194312+schok@users.noreply.github.com>
2023-06-04 10:15:58 -0700

    Add change grid size function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-function createDivs(divContainer) {
-    for (x=0; x<16; x++) {
+function createDivs(divContainer, size) {
+    for (x = 0; x < size; x++) {
         var divRow = document.createElement("div");
-            divRow.className = "row";
-            divRow.innerHTML = "";
-            divContainer.appendChild(divRow);
-        for (i=0; i<16; i++) {
+        divRow.className = "row";
+        divRow.innerHTML = "";
+        divContainer.appendChild(divRow);
+        for (i = 0; i < size; i++) {
             var divColumn = document.createElement("div");
             divColumn.className = "column";
             divColumn.innerHTML = "";
@@ -12,14 +12,18 @@ function createDivs(divContainer) {
         }
     }
 }
+function resetGrid() {
+    const gridColumn = document.querySelectorAll(".column");
+    gridColumn.forEach((div) => {
+        div.remove();
+    });
 
-divContainer = document.createElement("div");
-divContainer.id = "grid"
-main = document.querySelector("#main");
-main.appendChild(divContainer);
-createDivs(divContainer);
-
-function hover(divContainer){
+    const gridRow = document.querySelectorAll(".row");
+    gridRow.forEach((div) => {
+        div.remove();
+    })
+}
+function hover() {
     const grid = document.querySelectorAll(".column");
     console.log(grid);
     grid.forEach((div) => {
@@ -28,5 +32,33 @@ function hover(divContainer){
         });
     });
 }
-
-hover(divContainer);
+function adjustGrid(size) {
+    if (size == null) {
+        return;
+    }
+    else if (typeof(size) == "string") {
+        alert("Oops! The grid size must be numerical.")
+    }
+    else if (size > 100 || size < 1) {
+        alert("Oops! The grid size must be between 1 and 100.");
+        return;
+    }
+    else {
+        resetGrid();
+        createDivs(divContainer, size);
+        hover();
+    }
+}
+divContainer = document.createElement("div");
+divContainer.id = "grid"
+main = document.querySelector("#main");
+var button = document.createElement("button");
+button.innerHTML = "Change grid size";
+main.appendChild(divContainer);
+divContainer.append(button);
+createDivs(divContainer, 16);
+button.onclick = function () {
+    size = window.prompt("What grid size do you want?", "");
+    adjustGrid(size);
+}
+hover();
